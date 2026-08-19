1f14a473fbdc7730990bd3f0a48f01201241c0b8
dheepicaahg07 <dheepicaahgopinath@gmail.com>
2026-08-19 14:11:49 +0530

    Month

---
+++ oops/enum.java
@@ -0,0 +1,16 @@
+enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY
+}
+
+class enum {
+    public static void main(String[] args) {
+        Month month = Month.JANUARY;
+        System.out.println("Next Month: " + month)c
+    }
+        }
